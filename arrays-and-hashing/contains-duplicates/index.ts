7fbcb7de80e0de6c2f318be57fb75e17db509898
Andrey Radkevich <andrey@48pacific.com>
2022-04-29 16:15:59 +0300

    is-dublicate-only-for-strings-and-numbers

--- arrays-and-hashing/contains-duplicates/index.ts
+++ arrays-and-hashing/contains-duplicates/index.ts
@@ -2,7 +2,7 @@ const ex1 = [1, 2, 3, 1];
 const ex2 = [1, 2, 3, 4];
 const ex3 = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2];
 
-const isDuplicate: <T>(arr: T[]) => boolean = (arr) => {
+const isDuplicate: <T extends string | number>(arr: T[]) => boolean = (arr) => {
   const set = new Set();
 
   for (let element of arr) {
